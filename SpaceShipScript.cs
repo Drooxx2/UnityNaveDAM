d782af2aa09a18f91facb99b6981451cdcc4303c
Drooxx2 <75321607+Drooxx2@users.noreply.github.com>
2022-01-19 20:58:57 +0100

    ejercicio 1

---
+++ SpaceShipScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipScript : MonoBehaviour
+{
+    public int speed = 0;
+    public GameObject missilePrefab;
+    private Rigidbody2D _Rigidbody2D;
+    private Rigidbody2D rbmissile;
+    private GameObject missile;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Movement of the ship
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+        _Rigidbody2D.velocity = movement * speed;
+
+        //Limit of the ship on the screen
+        float xPos = Mathf.Clamp(transform.position.x, -6.5f, 6.5f);
+        float yPos = Mathf.Clamp(transform.position.y, -8f, 8f);
+        transform.position = new Vector2(xPos, yPos);
+
+        //Shoot a missile
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("Disparo!");
+            Vector2 missileSpawnPosition = new Vector2(transform.position.x, transform.position.y + 1.5f);
+            missile = Instantiate(missilePrefab, missileSpawnPosition, Quaternion.identity);
+            rbmissile = missile.GetComponent<Rigidbody2D>();
+            rbmissile.AddForce(new Vector2(0, 1000));
+        }
+    }
+}
